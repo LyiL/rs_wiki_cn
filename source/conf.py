f123c8cd92d3bc11cce4eec1921354671cf2c8ef
yilin.liu <yilin.liu@robosense.cn>
2025-02-08 18:37:46 +0800

    配置源代码链接

--- source/conf.py
+++ source/conf.py
@@ -76,3 +76,14 @@ html_theme_options = {
     'style_nav_header_background': 'red',
     'includehidden': False
 }
+
+# 配置源代码链接
+html_context = {
+    'display_github': True,  # 在页面上显示一个指向 GitHub 的链接
+    'github_user': 'LyiL',  # GitHub 用户名
+    'github_repo': 'rs_wiki_cn',  # GitHub 仓库名
+    'github_version': 'master/',  # GitHub 中文档的路径（通常是分支名和docs文件夹）
+    'conf_py_path': 'source/',  # conf.py 的路径，相对于 github_repo 的根目录
+    'root_path_parts':'introduction/',  # 示例
+    'source_suffix': '.md',  # 示例
+}
